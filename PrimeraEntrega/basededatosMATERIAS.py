436388bbc9b35b5998e98d6706c99aec23f61f61
jfiagar <jfiagar@unal.edu.co>
2020-10-09 11:00:32 -0500

    Actualizacion

---
+++ PrimeraEntrega/basededatosMATERIAS.py
@@ -0,0 +1,7 @@
+print("Bienvenido a la Base de datos de Estudiantes")
+
+
+# #CREAR LA BASE DE DATOS DE ESTUDIANTES
+# basedatosESTUDIANTES=open ("BDE.txt","a")  #Abrir archivo en modo adjuntar. Si el archivo no existe, crea un nuevo archivo.
+#
+# baseDatos.close()
