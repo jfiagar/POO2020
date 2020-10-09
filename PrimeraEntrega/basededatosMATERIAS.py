7f863166696b3078a131d04e5c9248c4f293234e
jfiagar <jfiagar@unal.edu.co>
2020-10-09 17:10:24 -0500

    Finalizado en esencia

--- PrimeraEntrega/basededatosMATERIAS.py
+++ PrimeraEntrega/basededatosMATERIAS.py
@@ -1,57 +1,98 @@
 import os.path
-print("Bienvenido a la Base de datos de Materias") #Saludo :v
-print("[0]  Ver la base de datos actual") #Imprime la primera opción
-print("[1]  Ingresar Materias a la base de datos") #Imprime la segunda opción
-print("[2]  Salir") #Imprime la tercera opción
-opcion_elegida=input("Ingrese el número de la opción: ")
-if opcion_elegida=="0":
-    if not os.path.isfile("BD-Materias.txt"):
-        print("Aún no existe una Base de datos")
+import os
+
+def menu_principalBDM():
+    print("Bienvenido a la Base de datos de Materias")  # Saludo :v
+    print("MENÚ PRINCIPAL")  # Imprime Menú principal
+    print("[0]  Ver la base de datos actual")  # Imprime la primera opción
+    print("[1]  Ingresar Materias a la base de datos")  # Imprime la segunda opción
+    print("[2]  Salir del programa")  # Imprime la tercera opción
+
+
+def menu_opcionesBDM():
+    print("[0]  Volver al menú principal")  # Imprime la primera opción
+    print("[1]  Salir del programa")  # Imprime la segunda opción
+
+
+while True:
+    menu_principalBDM()
+    opcion_elegida = input("Ingrese el número de la opción: ")
+    if opcion_elegida == "0":
+        print(
+            "----------------------------------------------------------------------------------------------------------------------------------------------------------------")
+        if not os.path.isfile("BD-Materias.txt"):
+            print("Aún no existe una Base de datos")
+        else:
+            with open("BD-Materias.txt", "r") as file:
+                print(file.read())
+        print(
+            "----------------------------------------------------------------------------------------------------------------------------------------------------------------")
+        menu_opcionesBDM()
+        opcion2 = input("Ingrese el número de la opción: ")
+        if opcion2 == "0":
+            continue
+        elif opcion2=="1":
+            print("Saliendo...")
+            break
+        else:
+            print("Opción no valida")
+
+
+
+    elif opcion_elegida == "1":
+        CM = input("Ingrese el código de la Materia: ")  # Código de la materia
+        NM = input("Ingrese el Nombre de la materia: ")  # Nombre de la materia
+        CF = input("Ingrese el Código facultad que dicta la Materia: ")  # Código facultad que la dicta
+        CD = input("Ingrese el Código departamento que dicta la Materia: ")  # Código departamento que la dicta
+        CC = input("Ingrese la Cantidad de créditos de la Materia: ")  # Cantidad de créditos
+        CMA = input(
+            "Ingrese el Código de la materia anterior obligatoria en el plan de estudios: ")  # Código materia anterior obligatoria en el plan de estudios
+        dict = {"CM": CM, "NM": NM, "CF": CF, "CD": CD, "CC": CC, "CMA": CMA}  # Creación del diccionario de la materia
+
+        if not os.path.isfile(
+                "BD-Materias.txt"):  # Verificación de que la base de datos exista usando "isfile" de la libreria os.path , si existe el archivo el valor es True, sino False .Por lo tanto si el archivo no existe entonces entra a la condición
+            BDM = open("BD-Materias.txt",
+                       "w")  # Este modo abre el archivo para escritura. Si el archivo no existe, crea un nuevo archivo.
+            numerodematerias = 1  # Establece el número de materias en 1
+            BDM.write(str(dict) + "\n")  # Escribe los datos de la primera materia
+            BDM.close()  # Cierra el archivo
+
+            with open('ContadorBD-Materias.txt',
+                      'w') as file:  # Este modo abre el archivo para escritura. Si el archivo no existe, crea un nuevo archivo.
+                file.write(
+                    str(numerodematerias))  # Escribe los datos  iniciales del numero de materias en la abse de datos
+
+
+        else:  # Si el archivo ya existe se entra a esta condicion
+            with open("BD-Materias.txt",
+                      "a") as file:  # Abrir archivo en modo adjuntar. Si el archivo no existe, crea un nuevo archivo.
+                file.write(str(dict) + "\n")  # Escribe los datos de la siguiente materia
+
+            with open('ContadorBD-Materias.txt',
+                      'r') as file:  # Abre el archivo del contador de materias en modo lectura
+                numerodematerias = file.read()  # Almacena el numero de materias leyendo el archivo
+            numerodematerias = int(
+                numerodematerias) + 1  # Como se hizo una adición a la base de datos de materias entonces se aumenta el contador en 1
+
+            with open('ContadorBD-Materias.txt',
+                      'w') as file:  # Este modo abre el archivo para escritura. Si el archivo no existe, crea un nuevo archivo.
+                file.write(str(
+                    numerodematerias))  # Actualiza el archivo sobreescribiendo el valor del numero de materias es decir borra los datos del archivo anterior y escribe de nuevo
+        menu_opcionesBDM()
+        opcion2 = input("Ingrese el número de la opción: ")
+        if opcion2 == "0":
+            continue
+        elif opcion2 == "1":
+            print("Saliendo...")
+            break
+        else:
+            print("Opción no valida")
+
+    elif opcion_elegida == "2":
+        print("Saliendo...")
+        break
     else:
-        with open("BD-Materias.txt","r") as file:
-            print(file.read())
-
-
-elif opcion_elegida=="1":
-    CM = input("Ingrese el código de la Materia: ")  # Código de la materia
-    NM = input("Ingrese el Nombre de la materia: ")  # Nombre de la materia
-    CF = input("Ingrese el Código facultad que dicta la Materia: ")  # Código facultad que la dicta
-    CD = input("Ingrese el Código departamento que dicta la Materia: ")  # Código departamento que la dicta
-    CC = input("Ingrese la Cantidad de créditos de la Materia: ")  # Cantidad de créditos
-    CMA = input(
-        "Ingrese el Código de la materia anterior obligatoria en el plan de estudios: ")  # Código materia anterior obligatoria en el plan de estudios
-    dict = {"CM": CM, "NM": NM, "CF": CF, "CD": CD, "CC": CC, "CMA": CMA}  # Creación del diccionario de la materia
-
-    if not os.path.isfile("BD-Materias.txt"):  # Verificación de que la base de datos exista usando "isfile" de la libreria os.path , si existe el archivo el valor es True, sino False .Por lo tanto si el archivo no existe entonces entra a la condición
-        BDM = open("BD-Materias.txt",
-                   "w")  # Este modo abre el archivo para escritura. Si el archivo no existe, crea un nuevo archivo.
-        numerodematerias = 1  # Establece el número de materias en 1
-        BDM.write(str(dict) + "\n")  # Escribe los datos de la primera materia
-        BDM.close()  # Cierra el archivo
-
-        with open('ContadorBD-Materias.txt','w') as file:  # Este modo abre el archivo para escritura. Si el archivo no existe, crea un nuevo archivo.
-            file.write(str(numerodematerias))  # Escribe los datos  iniciales del numero de materias en la abse de datos
-
-    else:  # Si el archivo ya existe se entra a esta condicion
-        with open("BD-Materias.txt","a") as file:  # Abrir archivo en modo adjuntar. Si el archivo no existe, crea un nuevo archivo.
-            file.write(str(dict) + "\n")  # Escribe los datos de la siguiente materia
-
-        with open('ContadorBD-Materias.txt', 'r') as file:  # Abre el archivo del contador de materias en modo lectura
-            numerodematerias = file.read()  # Almacena el numero de materias leyendo el archivo
-        numerodematerias = int(
-            numerodematerias) + 1  # Como se hizo una adición a la base de datos de materias entonces se aumenta el contador en 1
-
-        with open('ContadorBD-Materias.txt','w') as file:  # Este modo abre el archivo para escritura. Si el archivo no existe, crea un nuevo archivo.
-            file.write(str(
-                numerodematerias))  # Actualiza el archivo sobreescribiendo el valor del numero de materias es decir borra los datos del archivo anterior y escribe de nuevo
-elif opcion_elegida=="2":
-    print("saliendo")
-else:
-    print("Opción no valida")
-
-
-
-
+        print("Opción no valida")
 
 
 
--- PrimeraEntrega/pruebas.py
+++ PrimeraEntrega/pruebas.py
@@ -1,4 +1,77 @@
-with open("BD-Materias.txt", "r") as file:
-    print(file.read())
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
 
 
+def menu():
+    """
+    Función que limpia la pantalla y muestra nuevamente el menu
+    """
+    os.system('cls')  # NOTA para windows tienes que cambiar clear por cls
+    print("Selecciona una opción")
+    print("\t1 - primera opción")
+    print("\t2 - segunda opción")
+    print("\t3 - tercera opción")
+    print("\t9 - salir")
+
+
+while True:
+    # Mostramos el menu
+    menu()
+
+    # solicituamos una opción al usuario
+    opcionMenu = input("inserta un numero valor >> ")
+
+    if opcionMenu == "1":
+        print("")
+        input("Has pulsado la opción 1...\npulsa una tecla para continuar")
+    elif opcionMenu == "2":
+        print("")
+        input("Has pulsado la opción 2...\npulsa una tecla para continuar")
+    elif opcionMenu == "3":
+        print("")
+        input("Has pulsado la opción 3...\npulsa una tecla para continuar")
+    elif opcionMenu == "9":
+        break
+    else:
+        print("")
+        input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
